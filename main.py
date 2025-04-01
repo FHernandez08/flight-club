088eef089311d3423482464475223e32c5dc1cca
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-04-01 14:38:44 -0500

    Completed of project

--- main.py
+++ main.py
@@ -54,11 +54,17 @@ for destination in sheet_data:
         # )
         # SMS not working? Try whatsapp instead.
         notification_manager.send_whatsapp(
-            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+            message_body=f"Low price alert! Only ${cheapest_flight.price} to fly "
                          f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
                          f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
         )
 
+
+# =================================Retrieve your customer emails ==============================================
+
+customer_data = data_manager.get_customer_emails()
+customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
+print(f"Your email list includes {customer_email_list}")
 
 # ================================== Search for direct flights ================================================
 tomorrow = datetime.now() + timedelta(days=1)
@@ -89,3 +95,21 @@ for destination in sheet_data:
 
         cheapest_flight = find_cheapest_flight(stopover_flights)
         print(f"Cheapest indirect flight price is: ${cheapest_flight.price}")
+
+    # ====================================== Send Notifications and Emails ===================================
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only USD {cheapest_flight.price} to fly direct "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport},"\
+                      f"{cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        else:
+            message = f"Low price alert! Only USD {cheapest_flight.price} to fly "\
+                      f"{cheapest_flight.origin_airport} to {cheapest_flight.destination_airport},"\
+                      f"with {cheapest_flight.stops} stop(s) "\
+                      f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        notification_manager.send_whatsapp(message_body=message)
+
+        notification_manager.send_emails
--- notification_manager.py
+++ notification_manager.py
@@ -1,4 +1,6 @@
 import os
+import smtplib
+
 from twilio.rest import Client
 
 # Using a .env file to retrieve the phone numbers and tokens.
@@ -6,30 +8,13 @@ from twilio.rest import Client
 class NotificationManager:
 
     def __init__(self):
+        self.smtp_address = os.environ["EMAIL_PROVIDER_SMTP_ADDRESS"]
+        self.email = os.environ["MY_EMAIL"]
+        self.email_password = os.environ["MY_EMAIL_PASSWORD"]
+        self.whatsapp_number = os.environ["TWILIO_WHATSAPP_NUMBER"]
+        self.twilio_verified_number = os.environ["TWILIO_VERIFIED_NUMBER"]
         self.client = Client(os.environ['TWILIO_SID'], os.environ["TWILIO_AUTH_TOKEN"])
-
-    def send_sms(self, message_body):
-        """
-        Sends an SMS message through the Twilio API.
-        This function takes a message body as input and uses the Twilio API to send an SMS from
-        a predefined virtual number (provided by Twilio) to your own "verified" number.
-        It logs the unique SID (Session ID) of the message, which can be used to
-        verify that the message was sent successfully.
-
-        Parameters:
-        message_body (str): The text content of the SMS message to be sent.
-
-        Returns:
-        None
-
-        Notes:
-        - Ensure that `TWILIO_VIRTUAL_NUMBER` and `TWILIO_VERIFIED_NUMBER` are correctly set up in
-        your environment (.env file) and correspond with numbers registered and verified in your
-        Twilio account.
-        - The Twilio client (`self.client`) should be initialized and authenticated with your
-        Twilio account credentials prior to using this function when the Notification Manager gets
-        initialized.
-        """
+        self.connection = smtplib.SMTP(os.environ["EMAIL_PROVIDER_SMTP_ADDRESS"])
 
     # Is SMS not working for you or prefer whatsapp? Connect to the WhatsApp Sandbox!
     # https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
@@ -40,3 +25,14 @@ class NotificationManager:
             to=f'whatsapp:{os.environ["TWILIO_VERIFIED_NUMBER"]}'
         )
         print(message.sid)
+
+    def send_emails(self, email_list, email_body):
+        with self.connection:
+            self.connection.starttls()
+            self.connection.login(self.email, self.email_password)
+            for email in email_list:
+                self.connection.sendmail(
+                    from_addr=self.email,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{email_body}".encode('utf-8')
+                )
